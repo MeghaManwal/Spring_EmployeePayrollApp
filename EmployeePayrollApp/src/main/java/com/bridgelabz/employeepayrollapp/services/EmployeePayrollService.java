f650baf8ea29a698c771bb34728a1da4c5f21e73
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-06-19 11:25:32 +0530

    Added:To store Employee Payroll data into the List

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/services/EmployeePayrollService.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/services/EmployeePayrollService.java
@@ -11,38 +11,40 @@ import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
 @Service
 public class EmployeePayrollService implements IEmployeePayrollService {
 
+	private List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+	
 	@Override
 	public List<EmployeePayrollData> getEmployeePayrollData() {
-		List<EmployeePayrollData> empDataList = new ArrayList<>();
-		empDataList.add(new EmployeePayrollData(1, new EmployeePayrollDTO("Pankaj", 50000)));
-		return empDataList;
+		return employeePayrollList;
 	}
 
 	@Override
 	public EmployeePayrollData getEmployeePayrollDataById(int empId) {
-		EmployeePayrollData empData = null;
-		empData = new EmployeePayrollData(1, new EmployeePayrollDTO("Pankaj", 50000));
-		return empData;
+		return employeePayrollList.get(empId);
 	}
 
 	@Override
 	public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
 		EmployeePayrollData empData = null;
-		empData = new EmployeePayrollData(1, empPayrollDTO);
+		empData = new EmployeePayrollData(employeePayrollList.size()+1, empPayrollDTO);
+		employeePayrollList.add(empData);
 		return empData;
 	}
 
-	@Override
-	public EmployeePayrollData updateEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
-		EmployeePayrollData empData = null;
-		empData = new EmployeePayrollData(1, empPayrollDTO);
-		return empData;
+	public EmployeePayrollData updateEmployeePayrollData(int empId, EmployeePayrollDTO empPayrollDTO) {
+		EmployeePayrollData empData = this.getEmployeePayrollDataById(empId);
+		empData.setName(empPayrollDTO.name);
+		empData.setSalary(empPayrollDTO.salary);
+		employeePayrollList.set(empId, empData);
+		return null;
 	}
 
 	@Override
 	public void deleteEmployeePayrollData(int empId) {
-		
+		employeePayrollList.remove(empId);
 	}
+
+	
 	
 	
 
