db0320e5b8cb51097698fe72fa69bfb71967a71c
MeghaManwal <https://github.com/MeghaManwal/Basic-Calculator.git>
2021-06-26 10:35:34 +0530

    Added: To save employee Payroll Data to MySQL DB

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -1,14 +1,28 @@
 package com.bridgelabz.employeepayrollapp.model;
 
-
 import java.time.LocalDate;
 import java.util.List;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.JoinColumn;
+
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 
 import lombok.Data;
 
+@Entity
+@Table(name = "employee_payroll")
 public @Data class EmployeePayrollData {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int employeeId;
 	private String name;
 	private long salary;
@@ -16,9 +30,22 @@ public @Data class EmployeePayrollData {
 	public LocalDate startDate;
 	public String note;
 	public String profilePic;
+	
+	@ElementCollection
+	@CollectionTable(name="employee_department", joinColumns=@JoinColumn(name="id"))
+	@Column(name = "department")
 	public List<String> department; 
 	
 	public EmployeePayrollData() { }
+	
+	public EmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
+		this.updateEmployeePayrollData(empPayrollDTO);
+	}
+
+	
+	private void updateEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
+		
+	}
 
 	public EmployeePayrollData(int empId, EmployeePayrollDTO empPayrollDTO) {
 		this.employeeId = empId;
